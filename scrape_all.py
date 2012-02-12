c151e7f7477538ff296c511936a0f44695e03923
Andrew Pellett <anrope@gmail.com>
2012-02-12 02:10:57 -0500

    Refactored to separate parsing rows from requesting pages and db interaction. Still some issues to work out with bad time stamp formats.

---
+++ scrape_all.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python
+
+import argparse
+import sys
+import datetime
+import umssportscrawl
+
+#base urls for the different schools
+
+baseurl = {
+	'um': 'http://www.goblackbears.com',
+	'umfk': 'http://athletics.umfk.maine.edu'}
+
+#schedule urls for each specific sport
+
+sporturl = {
+	('umfk', 'm-basketball'): baseurl['umfk'] + '/schedule/1/',
+	('umfk', 'm-soccer'): baseurl['umfk'] + '/schedule/2/',
+
+	('umfk', 'w-basketball'): baseurl['umfk'] + '/schedule/7/',
+	('umfk', 'w-soccer'): baseurl['umfk'] + '/schedule/8/',
+	('umfk', 'w-volleyball'): baseurl['umfk'] + '/schedule/9/',
+
+	('um', 'm-baseball'): baseurl['um'] + '/sports/m-basebl/2011-12/schedule',
+	('um', 'm-basketball'): baseurl['um'] + '/sports/m-baskbl/2011-12/schedule',
+	('um', 'm-crosscountry'): baseurl['um'] + '/sports/m-xc/2011-12/schedule',
+	('um', 'm-football'): baseurl['um'] + '/sports/m-footbl/2011-12/schedule',
+	('um', 'm-icehockey'): baseurl['um'] + '/sports/m-hockey/2011-12/schedule',
+	('um', 'm-swimming'): baseurl['um'] + '/sports/m-swimonly/2011-12/schedule',
+	('um', 'm-track'): baseurl['um'] + '/sports/m-track/2011-12/schedule',
+
+	('um', 'w-basketball'): baseurl['um'] + '/sports/w-baskbl/2011-12/schedule',
+	('um', 'w-crosscountry'): baseurl['um'] + '/sports/w-xc/2011-12/schedule',
+	('um', 'w-fieldhockey'): baseurl['um'] + '/sports/w-fieldh/2011-12/schedule',
+	('um', 'w-icehockey'): baseurl['um'] + '/sports/w-hockey/2011-12/schedule',
+	('um', 'w-soccer'): baseurl['um'] + '/sports/w-soccer/2011-12/schedule',
+	('um', 'w-softball'): baseurl['um'] + '/sports/softbl/2011-12/schedule',
+	('um', 'w-swimming'): baseurl['um'] + '/sports/w-swimonly/2011-12/schedule',
+	('um', 'w-track'): baseurl['um'] + '/sports/w-track/2011-12/schedule',
+	}
+
+def schedule_identify_row(tag):
+	if (('class', 'schedule-row0') in tag.attrs or 
+		('class', 'schedule-row1') in tag.attrs):
+		if tag.td.string:
+			return True
+
+def schedule_parse_row(row):
+	ev = umssportscrawl.Event('%b %d, %Y', '%I:%M %p')
+	tds = row.findAll('td')
+
+	ev.date = str(tds[0].string)
+
+	if tds[1].b:
+		ev.home = True
+		ev.opponent = ' '.join(str(tds[1].b.string).split()[1:])
+		ev.location = ev.opponent
+	else:
+		ev.home = False
+		ev.opponent = ' '.join(str(tds[1].string).split()[1:])
+		ev.location = ev.opponent
+
+	if tds[2].string != 'TBA':
+		if str(tds[2].string) != '11 AM':
+			ev.time = str(tds[2].string)
+		else:
+			ev.time = '1:01 AM'
+	else:
+		ev.time = '4:04 AM'
+
+	# This nasty line gets rid of the annoying extra whitespace
+
+	ev.result = ' '.join(unicode(tds[3].string).split())
+
+	if tds[4].a:
+		ev.recap = str(tds[4].a.string)
+	else:
+		ev.recap = ''
+
+	return ev
+
+def conf_identify_row(tag):
+	pass
+
+def conf_parse_row(row):
+	pass
+
+def umfk_identify_row(tag):
+	"""
+	Passed to BeautifulSoup to pick out useful html tags
+
+	Returns True for useful tags
+	"""
+	#on the umfk men's basketball page, the useful tags are
+	#contained in <tr>'s with class = 'row1' or 'row2'.
+	if ('class', 'row1') in tag.attrs or ('class', 'row2') in tag.attrs:
+				return True
+
+def umfk_parse_row(row):
+	"""
+	Given a schedule row from a umfk athletics page,
+	return an Event with the proper fields parsed out
+	"""
+	ev = umssportscrawl.Event('%B %d, %Y', '%I:%M %p')
+	td = row.findAll('td')
+
+	ev.date = str(td[2].string)
+
+	if td[3].string != 'TBA':
+		ev.time = str(td[3].string)
+	else:
+		ev.time = '4:04 AM'
+
+	if td[4].a:
+		ev.opponent = str(td[4].a.string)
+	else:
+		ev.opponent = str(td[4].string)
+
+	ev.location = str(td[5].string)
+
+	if ev.location == 'Fort Kent, Maine':
+		ev.home = True
+	else:
+		ev.home = False
+
+	if td[6].a:
+		ev.result = str(td[6].a.string)
+		ev.recap = baseurl['umfk'] + str(td[6].a.attrs[0][1])
+	else:
+		ev.result = ''
+		ev.recap = ''
+
+	if td[7].a:
+		ev.video = str(td[7].a.attrs[0][1])
+
+	return ev
+
+def main(scrape_type):
+	umfk_sports = [t for t in sporturl.keys() if t[0] == 'umfk']
+
+	schedule_row_sports = [
+		('um', 'm-track'), ('um', 'm-crosscountry'), 
+		('um', 'm-swimming'), ('um', 'w-track'),
+		('um', 'w-crosscountry'), ('um', 'w-swimming'),
+		]
+
+	conf_row_sports = [
+		('um', 'm-baseball'), ('um', 'm-basketball'),
+		('um', 'm-icehockey'), ('um', 'm-football'),
+		('um', 'w-basketball'), ('um', 'w-fieldhockey'),
+		('um', 'w-icehockey'), ('um', 'w-soccer'),
+		('um', 'w-softball'),
+		]
+
+	umfk_scraper = umssportscrawl.Scraper(sporturl, umfk_identify_row, 
+		umfk_parse_row, umfk_sports, 'events')
+
+	schedule_row_scraper = umssportscrawl.Scraper(sporturl, 
+		schedule_identify_row, schedule_parse_row, 
+		schedule_row_sports, 'events')
+
+	conf_row_scraper = umssportscrawl.Scraper(sporturl, conf_identify_row,
+		conf_parse_row, conf_row_sports, 'events')
+
+	scrapers = [umfk_scraper, schedule_row_scraper]
+
+	print "Scraping events ({}):".format(scrape_type)
+
+	for s in scrapers:
+		if scrape_type == 'all':
+			s.update_events()
+		elif scrape_type == 'today':
+			s.update_todays_events()
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(
+		description='Scrapes sports schedules from UMS sites')
+	parser.add_argument(
+		'scrape_type', action='store', 
+		choices=['all', 'today'], help='Scrape type')
+	args = parser.parse_args()
+	
+	if args.scrape_type: # make sure user gave both arguments
+		main(args.scrape_type)
+	else:
+		parser.print_help()
--- umssportscrawl.py
+++ umssportscrawl.py
@@ -1,244 +1,185 @@
-#!/home/rking788/bin/python
-
 import requests
 from BeautifulSoup import BeautifulSoup
-import re
 import datetime
 import MySQLdb
-import sys
 
-now = datetime.datetime.now()
-
-TABLENAME = 'SportsEvents_UMFK'
-
-#base urls for the different schools
-baseurl = {
-	'um': 'http://www.goblackbears.com',
-	'umfk': 'http://athletics.umfk.maine.edu'}
-
-#schedule urls for each specific sport
-sporturl = {
-	('umfk', 'm-basketball'): baseurl['umfk'] + '/schedule/1/',
-	('umfk', 'm-soccer'): baseurl['umfk'] + '/schedule/2/',
-	('umfk', 'w-basketball'): baseurl['umfk'] + '/schedule/7/',
-	('umfk', 'w-soccer'): baseurl['umfk'] + '/schedule/8/',
-	('umfk', 'w-volleyball'): baseurl['umfk'] + '/schedule/9/'}
+mysqldb_host = 'localhost'
+mysqldb_user = ''
+mysqldb_password = ''
+mysqldb_database = 'umssportscrawl'
 
 #Pretty names for each school
+
 schoolname = {
-	'umfk': 'University of Maine Fort Kent'}
+	'umfk': 'University of Maine Fort Kent',
+	'um': 'University of Maine'
+	}
 
 #abbreviations corresponding to pretty school names
-schoolabbrev = {}
-for k,v in schoolname.iteritems():
-	schoolabbrev[v] = k
-#schoolabbrev = {v:k for k,v in schoolname.iteritems()}
 
-#regexs for fields we're interested in
-gooddata = {
-	'time': re.compile('\d?\d\:\d\d [aApP]M'),
-	'date': re.compile('[Jj]anuary|[Ff]ebruary|[Mm]arch|[Aa]pril|[Mm]ay|[Jj]une|[Jj]uly|[Aa]ugust|[Ss]eptember|[Oo]ctober|[Nn]ovember|[Dd]ecember \d?\d, \d\d\d\d'),
-	'location': re.compile('Maine|Massachusetts|Rhode Island|New York|Vermont|Virginia'),
-	'opponent': re.compile('Central Maine'),
-	'result': re.compile('^[WwLl] ')}
+schoolabbrev = dict((v,k) for k, v in schoolname.iteritems())
 
 class Event(object):
 	"""
 	Event class that performs the necessary manipulation
 	for various fields we're interested in.
 	"""
+	def __init__(self, date_format, time_format):
+		self._date = None
+		self._time = None
+		self.opponent = None
+		self.location = None
+		self.home = None
+		self.result = None
+		self.recap = None
+		self.video = None
 
+		self.date_format = date_format
+		self.time_format = time_format
+	
 	def _get_date(self):
 		return self._date
 
 	def _set_date(self, value):
 		"""date setter turns the date string into a date object"""
-		self._date = datetime.datetime.strptime(value, '%B %d, %Y').date()
+		self._date = datetime.datetime.strptime(value, self.date_format).date()
 
 	date = property(_get_date, _set_date)
-#	@property
-#	def date(self):
-#		return self._date
-
-#	@date.setter
-#	def date(self, d):
-#		"""date setter turns the date string into a date object"""
-#		self._date = datetime.datetime.strptime(d, '%B %d, %Y').date()
 
 	def _get_time(self):
 		return self._time
 
 	def _set_time(self, value):
 		"""time setter turns the time string into a time object"""
-		self._time = datetime.datetime.strptime(value, '%I:%M %p').time()
+		self._time = datetime.datetime.strptime(value, self.time_format).time()
 
 	time = property(_get_time, _set_time)
-
-#	@property
-#	def time(self):
-#		return self._time
-
-#	@time.setter
-#	def time(self, t):
-#		"""time setter turns the time string into a time object"""
-#		self._time = datetime.datetime.strptime(t, '%H:%M %p').time()
 
 	@property
 	def datetime(self):
 		return datetime.datetime.combine(self._date, self._time)
 	
 	def __str__(self):
-		#return str(self.datetime)
-		return 'event: {} at {} | {} | {} | {} | {} | {}'.format(self.date, self.time, self.opponent, self.location, self.home, self.result, self.recap)
+		return 'event: {} at {} | {} | {} | {} | {} | {}'.format(self.date, 
+			self.time, self.opponent, self.location, self.home, self.result, 
+			self.recap)
 
-def parse_umfk_tr(tag):
-	"""
-	Passed to BeautifulSoup to pick out useful html tags
+class Scraper(object):
+	def __init__(self, sporturl, identify_row, parse_row, sports, table_name):
+		self.sporturl = sporturl
+		self.identify_row = identify_row
+		self.parse_row = parse_row
+		self.sports = sports
+		self.table_name = table_name
 
-	Returns True for useful tags
-	"""
-	#on the umfk men's basketball page, the useful tags are
-	#contained in <tr>'s with class = 'row1' or 'row2'.
-	if ('class', 'row1') in tag.attrs or ('class', 'row2') in tag.attrs:
-				return True
-
-def umfk_parse_data(row):
-	"""
-	Given a schedule row from a umfk athletics page,
-	return an Event with the proper fields parsed out
-	"""
-	ev = Event()
-	td = row.findAll('td')
+	def get_db_cursor(self):
+		"""convenience function for opening a db cursor"""
+		db = MySQLdb.connect(host=mysqldb_host, user=mysqldb_user, 
+			passwd=mysqldb_password, db=mysqldb_database)
+		return db.cursor()
 	
-	ev.date = str(td[2].string)
-	
-	if td[3].string != 'TBA':
-		ev.time = str(td[3].string)
-	else:
-		ev.time = '11:11 PM'
+	def get_events(self, school, sport):
+		"""
+		Generic function that accepts a school and sport
+		and figures out how to scrape the right data
 
-	if td[4].a:
-		ev.opponent = str(td[4].a.string)
-	else:
-		ev.opponent = str(td[4].string)
+		Returns a list of Event objects
+		"""
+		print "getting events:", school, sport
 
-	ev.location = str(td[5].string)
+		#Grab the page and let BeautifulSoup parse it
+		#Manipulate headers to look like chrome, because some sites are
+		#serving different content based on user-agent
 
-	if ev.location == 'Fort Kent, Maine':
-		ev.home = True
-	else:
-		ev.home = False
+		r = requests.get(self.sporturl[(school, sport)], headers={'user-agent': 
+			'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, '
+			'like Gecko) Chrome/16.0.912.77 Safari/535.7'})
+		soup = BeautifulSoup(r.content,
+			convertEntities=BeautifulSoup.HTML_ENTITIES)
 
-	if td[6].a:
-		ev.result = str(td[6].a.string)
-		ev.recap = baseurl['umfk'] + str(td[6].a.attrs[0][1])
-	else:
-		ev.result = ''
-		ev.recap = ''
-
-	if td[7].a:
-		ev.video = str(td[7].a.attrs[0][1])
-
-	return ev
-
-def get_events(school, sport):
-	"""
-	Generic function that accepts a school and sport
-	and figures out how to scrape the right data
-
-	Returns a list of Event objects
-	"""
-	print "getting events:", school, sport
-
-	#Grab the page and let BeautifulSoup parse it
-	r = requests.get(sporturl[(school, sport)])
-	soup = BeautifulSoup(r.content)
-
-	#Testing source so we don't kill the web servers
-	#fin = open('umfk-m-basketball.html')
-	#soup = BeautifulSoup(fin.read())
-
-	#Use a school/sport-tailored function to grab useful tags
-	tags = soup.findAll(parse_umfk_tr)
-
-	events = []
-	#Iterate over matched tags
-	for tag in tags:
-		ev = umfk_parse_data(tag)
-		ev.hometeam = schoolname[school]
-		ev.sport = sport
-		events.append(ev)
-	
-	return events
-
-def get_db_cursor():
-	"""convenience function for opening a db cursor"""
-	db = MySQLdb.connect(host = 'umainedb.mainelyapps.com', user = 'umaine', passwd = 'fjav77*', db = 'mainelyapps_umaine')
-	return db.cursor()
-
-def save_events(events):
-	"""
-	given some events either:
-	- update the recap, result, opponent/teamB, modifiedDateTime fields
-		if there is already an entry for the date and sport
-	- add a new row for the new event
-	"""
-	cursor = get_db_cursor()
-
-	for ev in events:
-		#check to see if there's already a row for this event
-		cursor.execute('select * from ' + TABLENAME + ' where dateTime = %s and sport = %s', (ev.datetime, ev.sport))
+		#Use a school/sport-tailored function to grab useful tags
 		
-		if cursor.fetchone():
-			#if there is an existing row, just update it
-			cursor.execute('''update ''' + TABLENAME + ''' set
-				recaplink = %s,
-				result = %s,
-				teamB = %s,
-				modifiedDateTime = %s
-				where dateTime = %s and sport = %s''', (ev.recap, ev.result, ev.opponent, datetime.datetime.now(), ev.datetime, ev.sport))
-		else:
-			#if there isn't an existing row, insert a new one
-			beginYear = str(now.year)
-			endYear = str(int(beginYear[2:]) + 1)
-			yearRange = beginYear + '-' + endYear
-			row = (ev.datetime, ev.home, ev.recap, ev.result, ev.sport, ev.hometeam, ev.opponent, yearRange, datetime.datetime.now())
-			cursor.execute('insert into ' + TABLENAME + ' (dateTime, home, recapLink, result, sport, teamA, teamB, yearRange, modifiedDateTime) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)', row)
+		tags = soup.findAll(self.identify_row)
 
-	cursor.close()
+		events = []
 
-def get_todays_sports():
-	"""
-	check db to see which sports have events today
-	return a list of tuples of (school, sport)
-	"""
-	cursor = get_db_cursor()
-	cursor.execute('select teamA, sport from ' + TABLENAME + ' where date(dateTime) = date(now())')
-	rows = cursor.fetchall()
-	cursor.close()
-	return rows
+		#Iterate over matched tags
 
-def update_all_events():
-	"""rescrape all events"""
-	for k in sporturl.keys():
-		school, sport = k
-		events = get_events(school, sport)
-		save_events(events)
+		for tag in tags:
+			ev = self.parse_row(tag)
+			ev.hometeam = schoolname[school]
+			ev.sport = sport
+			events.append(ev)
+		
+		return events
 
-def update_todays_events():
-	"""rescrape just todays events"""
-	todays_sports = get_todays_sports()
-	for event in todays_sports:
-		school, sport = event
-		school = schoolabbrev[school]
-		events = get_events(school, sport)
-		save_events(events)
+	def save_events(self, events):
+		"""
+		given some events either:
+		- update the recap, result, opponent/teamB, modifiedDateTime fields
+			if there is already an entry for the date and sport
+		- add a new row for the new event
+		"""
+		cursor = self.get_db_cursor()
 
-def main(argv):
-	if argv[1] == 'all':
-		update_all_events()
-	elif argv[1] == 'today':
-		update_todays_events()
+		for ev in events:
 
-main(sys.argv)
+			# check to see if there's already a row for this event.
+			# an event is uniquely identified by a date, time, and sport.
+			# all other fields can be changed on update
 
+			cursor.execute('select * from ' + self.table_name +
+				' where dateTime = %s and sport = %s', (ev.datetime, ev.sport))
+			
+			if cursor.fetchone():
+
+				#if there is an existing row, just update it
+
+				cursor.execute('update ' + self.table_name + ' set '
+					'recaplink = %s, result = %s, teamB = %s, '
+					'modifiedDateTime = %s where dateTime = %s and sport = %s', 
+					(ev.recap, ev.result, ev.opponent, datetime.datetime.now(), 
+					ev.datetime, ev.sport))
+			else:
+
+				# if there isn't an existing row, insert a new one
+
+				beginYear = str(datetime.datetime.now().year)
+				endYear = str(int(beginYear[2:]) + 1)
+				yearRange = beginYear + '-' + endYear
+				cursor.execute('insert into ' + self.table_name +
+					' (dateTime, home, recapLink, result, sport, teamA, teamB, '
+					'yearRange, modifiedDateTime) values (%s, %s, %s, %s, %s, '
+					'%s, %s, %s, %s)', (ev.datetime, ev.home, ev.recap, 
+					ev.result, ev.sport, ev.hometeam, ev.opponent, 
+					yearRange, datetime.datetime.now()))
+
+		cursor.close()
+
+	def get_todays_sports(self):
+		"""
+		check db to see which sports have events today
+		return a list of tuples of (school, sport)
+		"""
+		cursor = self.get_db_cursor()
+		cursor.execute('select teamA, sport from ' + self.table_name +
+			' where date(dateTime) = date(now())')
+		rows = cursor.fetchall()
+		cursor.close()
+		return rows
+
+	def update_events(self):
+		"""rescrape all events"""
+		for k in self.sports:
+			school, sport = k
+			events = self.get_events(school, sport)
+			self.save_events(events)
+
+	def update_todays_events(self):
+		"""rescrape just todays events"""
+		todays_sports = self.get_todays_sports()
+		for event in todays_sports:
+			school, sport = event
+			school = schoolabbrev[school]
+			events = self.get_events(school, sport)
+			self.save_events(events)
